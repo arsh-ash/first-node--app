328651b0e970efb27099e0a9540bcc4387ba701e
Mohd Arsh <70385742+arsh-ash@users.noreply.github.com>
2021-11-27 16:16:28 +0530

    added express router and controller

---
+++ controller/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home=function(req,res){
+    return res.send("<h1>we are on home page using routes using middleware using everything</h1>");
+}
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require("express");
 const app=express();
 port=8000;
+//use express router
+const router=require("./routes");
+
+//middleware home page chlte is router ki index file m cla jaega
+app.use("/",router);
 
 
 
