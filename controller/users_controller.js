511d1aee2f2cb3ff7d45fa38b3b2b81655a45212
Mohd Arsh <70385742+arsh-ash@users.noreply.github.com>
2021-12-02 11:11:29 +0530

    commite extra changes

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,22 +1,8 @@
 const { response } = require("express");
-const { findById } = require("../model/user");
 const User = require("../model/user");
 // const user=require("../model/user");
 module.exports.profile = function (req, res) {
   res.render("profile");
-
-// if(res.cookies.user_id){
-//     console.log("hiiiiii");
-//     findById(res.cookies.user_id,function(err,user){
-//         if(user){
-//             return res.render("profile");
-//         }
-//        return res.redirect("/users/signin");
-//     })
-// }
-// else{
-//     return res.redirect("/users/signin");
-// }
 };
 
 module.exports.signIn = function (req, res) {
@@ -63,34 +49,6 @@ module.exports.create = function (req, res) {
 };
 
 module.exports.createSession = function (req, res) {
-    //find user
-    User.findOne({
-        email:req.body.email
-    },function(err,user){
-        if(err){
-            console.log("error in finding user");
-            return;
-        }
-        //handle user found
-        if(user){
-            //handle password which doesnot match
-            if(user.password!=req.body.password){
-                return res.redirect("back");
-
-            }
-    
-    //handle session creation (cookies)
-    res.cookie("user_id",user.id);
-    return res.redirect("/users/profile")
-
-        }else{
-            return res.redirect("back");
-
-        }
-    })
-
-
     
 
-    //handle user not found
 };
