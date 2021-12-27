e5cc6226b1218196cb9a7a466893548da9e2d4ba
Mohd Arsh <70385742+arsh-ash@users.noreply.github.com>
2021-12-27 14:36:23 +0530

    added post deleting feature using routes and controllers

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,24 +1,50 @@
-const express=require('express');
-const Post=require("../model/post")
+const express = require("express");
+const Post = require("../model/post");
+const Comments=require("../model/comments")
+const { post } = require("../routes");
+
+module.exports.create = function (req, res) {
+  Post.create(
+    {
+      content: req.body.content,
+      user: req.user._id,
+    },
+    function (err, post) {
+      if (err) {
+        console.log("error in  creating post");
+        return;
+      }
+      console.log("everything is good");
+      return res.redirect("back");
+    }
+  );
+};
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+
+        if(post){
+            if(post.user==req.user.id){
+                post.remove();
+                console.log("deleting post")
 
 
-module.exports.create=function(req,res){
-      Post.create({
-        content:req.body.content,
-        user:req.user._id,
-    },function(err,post){
+                Comments.deleteMany({post:req.params.id},function(err){
+                    console.log(req.params);
+                    // return res.redirect("back");
+                })
 
-        if(err){
-            console.log("error in  creating post");
-            return
+
+            }
+            return res.redirect("back");
         }
-        console.log("everything is good");
-        return res.redirect("back");
+        else{
+            console.log("not deleting");
+            return res.redirect("back");
 
-
+        }
 
     })
-
 
 
 
